c5b414dc6123b955a5693ca435e02b224386fe3b
Mike Wright <mtw5128@gmail.com>
2020-04-06 17:07:25 -0400

    sliceing challenge

--- ch02.py
+++ ch02.py
@@ -2,10 +2,10 @@
 
 col_list = ["blue", "Columbus"]
 
-col_list.append("1492")
+col_list.append(1492)
 
-name = input("What is your name?")
+name = input("What is your name? ")
 
-print(f"In {col_list[2]}, {col_list[1]} sailed the ocean blue. {name} fell off the boat.")
+print(f"In {col_list[2]}, {col_list[1]} sailed the ocean {col_list[0]}. {name} fell off the boat.")
 
 
